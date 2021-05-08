7f507828557f45cacad17928e9d82d397abb5377
BenjaminBarret <bbarret.dev@gmail.com>
2021-05-08 11:59:38 +0400

    ADD: cartActions-Constants-Reducers.js || FEATURES: addToCart - localStorageCartItems

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,37 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               e-commerce
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Panier</a>
-            <a href="/signin">Se connecter</a>
+            <Link to="/cart">Panier
+              {cartItems.length > 0 &&(
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Se connecter</Link>
           </div>
         </header>
         <main>
-          <Route path="/cart/:id?" component={CartScreen}/>
-          <Route path="/product/:id" component={ProductScreen}/>
-          <Route path="/" component={HomeScreen} exact/>
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/" component={HomeScreen} exact />
         </main>
         <footer className="row center">Tous droits réservés.</footer>
       </div>
