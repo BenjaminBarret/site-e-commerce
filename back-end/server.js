facfce43f91822b6e07d1037a9b0a12baf9a7a52
BenjaminBarret <bbarret.dev@gmail.com>
2021-05-10 22:14:45 +0400

    ADD: back-end/models/ - back-end/routers/ || FEATURES: connection to mongodb - modelUser

--- back-end/server.js
+++ back-end/server.js
@@ -1,27 +1,40 @@
-import express from 'express';
-import data from './data.js'
+import express from "express";
+import mongoose from "mongoose";
+import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/site-e-commerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find(x => x._id === req.params.id);
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: 'Produit introuvable'});
+    res.status(404).send({ message: "Produit introuvable" });
   }
 });
 
-app.get('/api/products', (req, res) => {
+app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.get('/', (req, res) => {
-  res.send('Server is ready !');
+app.use("/api/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready !");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,() => {
+app.listen(port, () => {
   console.log(`Serve is running at http://localhost:${port}`);
 });
