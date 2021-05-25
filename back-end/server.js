d2f4af9d9eb45efdad28b67632916aa7f8815fbb
BenjaminBarret <bbarret.dev@gmail.com>
2021-05-25 15:39:13 +0400

    FIX: path front-end*

--- back-end/server.js
+++ back-end/server.js
@@ -41,9 +41,9 @@ app.get("/api/config/paypal", (req, res) => {
 // });
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/front-end/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/front-end/build/index.html'))
 );
 
 app.use((err, req, res, next) => {
